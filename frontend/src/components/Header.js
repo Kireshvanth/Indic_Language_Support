fe768936c827e4b2a0b67992a1f6c09a6849c653
jeyam03 <64417749+jeyam03@users.noreply.github.com>
2024-02-10 16:40:28 +0530

    Search Context

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { EN_INDIC, OCR_URL } from '../api';
 import axios from 'axios';
 import { Outlet } from 'react-router-dom';
+
+export const SearchTextContext = createContext();
 
 const Header = () => {
   const navigate = useNavigate();
@@ -87,89 +89,90 @@ const Header = () => {
 
   return (
     <div className="flex flex-col gap-8 bg-slate-300 py-16 w-screen min-h-screen">
-      <div className='flex flex-row gap-2 w-full px-20'>
-        <button
-          className="bg-white bg-opacity-20 backdrop-blur-lg rounded-2xl px-20 py-4 min-w-fit h-fit border-2 border-gray-200"
-          onClick={() => {
-            navigate('/');
-          }}
-        >
-          <h1 className="text-3xl font-semibold font-poppins" tkey='title'>Build For</h1>
-          <h1 className="text-3xl font-semibold font-poppins -ml-6" tkey='title2'>Bharat</h1>
-          <img src="/assets/shopping-cart.webp" alt="Shopping Cart" className="absolute -top-[50%] -left-[20%] w-40" />
-        </button>
+      <SearchTextContext.Provider value={searchText}>
+        <div className='flex flex-row gap-2 w-full px-20'>
+          <button
+            className="bg-white bg-opacity-20 backdrop-blur-lg rounded-2xl px-20 py-4 min-w-fit h-fit border-2 border-gray-200"
+            onClick={() => {
+              navigate('/');
+            }}
+          >
+            <h1 className="text-3xl font-semibold font-poppins" tkey='title'>Build For</h1>
+            <h1 className="text-3xl font-semibold font-poppins -ml-6" tkey='title2'>Bharat</h1>
+            <img src="/assets/shopping-cart.webp" alt="Shopping Cart" className="absolute -top-[50%] -left-[20%] w-40" />
+          </button>
 
-        <div className='flex flex-col gap-2 w-3/4'>
-          <div className='flex flex-row gap-2 w-full'>
-            <div className="bg-white bg-opacity-20 backdrop-blur-lg rounded-2xl p-2 w-[60%] h-16 border-2 border-gray-200 flex flex-row gap-4">
-              <img src="/assets/search-icon.webp" alt="Search Icon" className='w-10' />
-              <input type="text" placeholder="Search" className="bg-transparent border-none w-full focus:outline-none" tkey='Search' value={searchText}
-                onChange={(event) => setSearchText(event.target.value)}
-              />
-            </div>
-            <div className="bg-white bg-opacity-20 backdrop-blur-lg rounded-2xl p-2 w-fit h-16 border-2 border-gray-200">
-              <img src="/assets/mic.webp" alt="Mic Icon" className='w-10' />
-            </div>
-            <div className='relative'>
-              <label htmlFor="file-input">
-                <input
-                  id="file-input"
-                  type="file"
-                  onChange={handleFileChange}
-                  className="hidden"
+          <div className='flex flex-col gap-2 w-3/4'>
+            <div className='flex flex-row gap-2 w-full'>
+              <div className="bg-white bg-opacity-20 backdrop-blur-lg rounded-2xl p-2 w-[60%] h-16 border-2 border-gray-200 flex flex-row gap-4">
+                <img src="/assets/search-icon.webp" alt="Search Icon" className='w-10' />
+                <input type="text" placeholder="Search" className="bg-transparent border-none w-full focus:outline-none" tkey='Search' value={searchText}
+                  onChange={(event) => setSearchText(event.target.value)}
                 />
-                <div className="bg-white bg-opacity-20 backdrop-blur-lg rounded-2xl p-2 w-fit h-16 border-2 border-gray-200 cursor-pointer">
-                  <img src="/assets/camera2.png" alt="Camera Icon" className='w-10' />
-                </div>
-              </label>
-              {uploading && (
-                <div className={`bg-white bg-opacity-20 backdrop-blur-lg rounded-xl px-4 py-2 w-fit border-2 ${errorMessage === '' ? 'border-gray-200' : 'border-red-500'} absolute bottom-[115%] -left-[25%] flex flex-row gap-1 z-20 text-sm font-light`}>
-                  <span class="relative flex h-3 w-3 mt-1 mr-1">
-                    <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-                    <span class="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
-                  </span>
-                  {uploading && <p>Uploading </p>}
-                  {file && <p className='whitespace-nowrap'>{file.name}</p>}
-                </div>
-              )}
-            </div>
-            <div className="bg-white bg-opacity-20 backdrop-blur-lg rounded-2xl p-2 w-fit h-16 border-2 border-gray-200 flex flex-row gap-2">
-              <img src="/assets/translate.webp" alt="Translate Icon" className='w-10' />
-              <select
-                className="w-24 bg-transparent border-none leading-tight focus:outline-none"
-                value={currentLang}
-                onChange={(event) => {
-                  setCurrentLang(event.target.value);
+              </div>
+              <div className="bg-white bg-opacity-20 backdrop-blur-lg rounded-2xl p-2 w-fit h-16 border-2 border-gray-200">
+                <img src="/assets/mic.webp" alt="Mic Icon" className='w-10' />
+              </div>
+              <div className='relative'>
+                <label htmlFor="file-input">
+                  <input
+                    id="file-input"
+                    type="file"
+                    onChange={handleFileChange}
+                    className="hidden"
+                  />
+                  <div className="bg-white bg-opacity-20 backdrop-blur-lg rounded-2xl p-2 w-fit h-16 border-2 border-gray-200 cursor-pointer">
+                    <img src="/assets/camera2.png" alt="Camera Icon" className='w-10' />
+                  </div>
+                </label>
+                {uploading && (
+                  <div className={`bg-white bg-opacity-20 backdrop-blur-lg rounded-xl px-4 py-2 w-fit border-2 ${errorMessage === '' ? 'border-gray-200' : 'border-red-500'} absolute bottom-[115%] -left-[25%] flex flex-row gap-1 z-20 text-sm font-light`}>
+                    <span class="relative flex h-3 w-3 mt-1 mr-1">
+                      <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                      <span class="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+                    </span>
+                    {uploading && <p>Uploading </p>}
+                    {file && <p className='whitespace-nowrap'>{file.name}</p>}
+                  </div>
+                )}
+              </div>
+              <div className="bg-white bg-opacity-20 backdrop-blur-lg rounded-2xl p-2 w-fit h-16 border-2 border-gray-200 flex flex-row gap-2">
+                <img src="/assets/translate.webp" alt="Translate Icon" className='w-10' />
+                <select
+                  className="w-24 bg-transparent border-none leading-tight focus:outline-none"
+                  value={currentLang}
+                  onChange={(event) => {
+                    setCurrentLang(event.target.value);
+                  }}
+                >
+                  {Object.keys(indicLanguages)?.map((language, index) => (
+                    <option key={index} value={language}>
+                      {language}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <button
+                className="bg-white bg-opacity-20 backdrop-blur-lg rounded-2xl p-2 w-fit h-16 border-2 border-gray-200 flex items-center"
+                onClick={() => {
+                  navigate('/profile');
                 }}
               >
-                {Object.keys(indicLanguages)?.map((language, index) => (
-                  <option key={index} value={language}>
-                    {language}
-                  </option>
-                ))}
-              </select>
+                <img src="/assets/profile.webp" alt="Profile Icon" className='w-8 mx-1' />
+              </button>
             </div>
-            <button
-              className="bg-white bg-opacity-20 backdrop-blur-lg rounded-2xl p-2 w-fit h-16 border-2 border-gray-200 flex items-center"
-              onClick={() => {
-                navigate('/profile');
-              }}
-            >
-              <img src="/assets/profile.webp" alt="Profile Icon" className='w-8 mx-1' />
-            </button>
-          </div>
 
-          <div className='flex flex-row gap-2 w-full overflow-auto no-scrollbar'>
-            {
-              searchItems.map((item, index) => (
-                <SearchItem key={index} item={item} />
-              ))
-            }
+            <div className='flex flex-row gap-2 w-full overflow-auto no-scrollbar'>
+              {
+                searchItems.map((item, index) => (
+                  <SearchItem key={index} item={item} />
+                ))
+              }
+            </div>
           </div>
         </div>
-      </div>
-
-      <Outlet />
+        <Outlet />
+      </SearchTextContext.Provider>
     </div>
   )
 }
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,92 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import axios from 'axios';
 import { CHAT_URL } from '../api';
+import { SearchTextContext } from '../components/Header';
 
 const HomeScreen = () => {
-  const products = [
+  const [products, setProducts] = useState([
+    {
+      img: 'https://www.91-cdn.com/hub/wp-content/uploads/2024/01/oneplus-12-new-image-1-1-1.jpg',
+      title: 'OnePlus 12',
+      price: '69,999'
+    },
+    {
+      img: 'https://www.digitaltrends.com/wp-content/uploads/2023/02/macbook-pro-14-m2-max.jpg?p=1',
+      title: 'MacBook Pro 14"',
+      price: '1,24,999'
+    },
+    {
+      img: 'https://img.freepik.com/premium-photo/mockup-laptop-presented-digital-art-style-contemporary-product-photography-generative-ai_527096-23348.jpg',
+      title: 'Vivobook S 15"',
+      price: '80,999'
+    },
+    {
+      img: 'https://fdn.gsmarena.com/imgroot/news/20/07/sony-xb-700/-727w2/gsmarena_011.jpg',
+      title: 'Sony WF-XB700',
+      price: '7,999'
+    },
+    {
+      img: 'https://www.macworld.com/wp-content/uploads/2023/11/Apple-Watch-Series-8_review_5-2.jpg?quality=50&strip=all',
+      title: 'Apple Watch SE',
+      price: '29,900'
+    },
+    {
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaAfd8klka4Vp8TisGcuZITJq5TFTB5gAvlBOVknIth6Od2rO9xY7KeAPZjGXi5Tk7vQ8&usqp=CAU',
+      title: 'Canon EOS 1500D',
+      price: '29,999'
+    },
+    {
+      img: 'https://cdn.thewirecutter.com/wp-content/media/2023/11/gamingconsoles-2048px-00633.jpg',
+      title: 'Sony PlayStation 5',
+      price: '49,990'
+    },
+    {
+      img: 'https://images.news18.com/ibnlive/uploads/2022/05/lg-rollable-tv-india-16534016784x3.jpg',
+      title: 'LG Rollable 8K OLED TV',
+      price: '1,19,999'
+    },
+    {
+      img: 'https://www.aptx.com/sites/default/files/2020-07/vivo-tws-neo.jpg',
+      title: 'Vivo Neo TWS',
+      price: '2,999'
+    },
+    {
+      img: 'https://www.stereo.com.sg/pub/media/catalog/product/cache/5fc3014ed970f95b2ceaf2a9ddc3aa5f/1/3/13122023_33707_pm_watch_05.jpg',
+      title: 'Nothing CMF Smart Watch',
+      price: '20,499'
+    },
+    {
+      img: 'https://pimcdn.sharafdg.com/cdn-cgi/image/width=600,height=600,fit=pad/images/S300849631_1?1701084621',
+      title: 'Realme Buds Air 3',
+      price: '3,499'
+    },
+    {
+      img: 'https://media.comicbook.com/2020/04/nintendo-switch-1218158.jpeg',
+      title: 'Nintendo Switch',
+      price: '24,499'
+    },
+    {
+      img: 'https://photofocus.com/reviews/putting-the-tamron-17-28mm-f-2-8-lens-through-its-paces/attachment/julie-powell_1728-2/',
+      title: 'Sony Alpha 1 ',
+      price: '1,49,999'
+    },
+    {
+      img: 'https://static.wixstatic.com/media/661f56_f5696ff31940480e8e42d1e2e58d59d6.gif',
+      title: 'Canon EOS R50 ',
+      price: '95,999'
+    },
+    {
+      img: 'https://static.wixstatic.com/media/661f56_f5696ff31940480e8e42d1e2e58d59d6.gif',
+      title: 'Canon EOS R50 ',
+      price: '95,999'
+    },
+    {
+      img: 'https://images.fonearena.com/blog/wp-content/uploads/2024/01/HONOR-Magic-6-and-Magic-6-Pro-1024x812.jpg',
+      title: 'Honor Magic 6 Pro',
+      price: '66,390'
+    }
+  ]);
+  const mainProducts = [
     {
       img: 'https://www.91-cdn.com/hub/wp-content/uploads/2024/01/oneplus-12-new-image-1-1-1.jpg',
       title: 'OnePlus 12',
@@ -85,7 +168,6 @@ const HomeScreen = () => {
       price: '66,390'
     }
   ];
-
   const [productCount, setProductCount] = useState(products.map(() => 0));
   const [chatBotOpen, setChatBotOpen] = useState(false);
   const [userChat, setUserChat] = useState([]);
@@ -126,6 +208,19 @@ const HomeScreen = () => {
       });
     }
   }, [botChat, userChat]);
+
+  const searchText = useContext(SearchTextContext);
+
+  useEffect(() => {
+    if (searchText) {
+      setProducts((prev) => {
+        return prev.filter((product) => product.title.toLowerCase().includes(searchText.toLowerCase()));
+      });
+    }
+    if (searchText === '') {
+      setProducts(mainProducts);
+    }
+  }, [searchText]);
 
   return (
     <div className="flex flex-col gap-8">
@@ -241,7 +336,7 @@ const Product = ({ img, title, price, productState, index }) => {
   const [productCount, setProductCount] = productState;
 
   return (
-    <div className={`min-w-64 flex flex-col gap-2 ${index === 0 && 'ml-20'}`}>
+    <div className={`min-w-64 max-w-64 flex flex-col gap-2 ${index === 0 && 'ml-20'}`}>
       <img src={img} alt={title} className="w-full h-40 object-cover rounded-lg shadow-md" />
       <div className="">
         <div className='flex flex-row gap-2 justify-between w-full'>
